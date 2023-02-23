ebfb2b31baa77e69c1146d3469626364b4d05bc3
vartlbox <125939870+vartlbox@users.noreply.github.com>
2023-02-23 18:10:19 +0300

    Add files via upload

---
+++ book.js
@@ -0,0 +1,33 @@
+document.getElementById("myButton").onclick = function(){
+
+
+
+
+
+
+    
+var myText1  = document.getElementById("myText1").value ; 
+var myText2  = document.getElementById("myText2").value ; 
+
+
+var reslt =  myText1 * myText2
+
+
+
+
+document.write(reslt)
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
